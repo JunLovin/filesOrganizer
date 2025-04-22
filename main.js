87a6ff0f0c20edb232fc3ef4337daecfb7b18f36
Mathias Rendon <mathiassaid7@gmail.com>
2025-04-22 10:20:54 -0500

    refactor(main.js): update window dimensions and add comments
    Increase the window dimensions to 1980x1080 for better usability and add descriptive comments to improve code readability

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
+// - Aplicación creada con electron
+// ? Importamos dos módulos de electron
+// ? app: Controla la vida de la aplicación
+// ? BrowserWindow: Crea y controla las ventanas de la aplicación
 const { app, BrowserWindow } = require('electron')
 
+// - Creamos la ventana de la aplicación
 const createWindow = () => {
+    // ? Creamos una instancia de BrowserWindow y lo guardamos en la variable win
+    // ? Le asignamos un ancho y alto de 800px y 600px respectivamente
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1980,
+        height: 1080
     })
 
+    // ? Cargamos el archivo index.html en la ventana
     win.loadFile('./src/index.html')
 }
 
+// - Iniciamos la aplicación
+// ? .whenReady es el evento 'ready' de nodejs, pero utilizamos el método whenReady de electron para evitar problemas con las promesa
 app.whenReady().then(() => {
+    // ? Llamamos a la función createWindow para crear la ventana de la aplicación
     createWindow()
 })
