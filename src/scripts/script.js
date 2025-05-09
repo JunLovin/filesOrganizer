636602027a1e42bfa1eb920b07738ce14354527d
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-09 16:38:26 -0500

    refactor(scripts): replace jQuery with vanilla JavaScript
    The jQuery code was replaced with vanilla JavaScript to improve performance and reduce dependency on external libraries. Added event listeners for form submission and button clicks to open a GitHub profile in a new tab.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,12 @@
-$(document).ready(() => {
-    $('#header-title').on('click', () => {
-        $('#header-title').text("Test JQuery")
-    })
+const form = document.querySelector('.form-message');
+const gitBtn = document.querySelectorAll('.git-btn');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 })
+
+for (let i = 0; i < gitBtn.length; i++) {
+    gitBtn[i].addEventListener('click', () => {
+        window.open('https://github.com/JunLovin', '_target')
+    })
+}
